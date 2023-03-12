66d00f93957e6bb152da48ccfc55b298179f8f4c
Paula Sousa <110301198+PaulaSousa2014@users.noreply.github.com>
2023-03-12 18:33:44 +0100

    Comentarios

--- src/ud02b/ClassInteger1.java
+++ src/ud02b/ClassInteger1.java
@@ -3,9 +3,15 @@ package ud02b;
 public class ClassInteger1 {
 
 	public static void main (String args[]) {
+		
+		//Creación de un objeto Integer usando el segundo constructor
 		Integer num1=new Integer("125");
+		
+		//Creación de un objeto Integer usando el primer constructor
 		Integer num2=new Integer(20);
 		
+		//Obtención del entero que almacena cada objeto Integer
+		//Si no se hace esto, las líneas siguientes causarian error de compilacion
 		int n1= num1.intValue();
 		int n2= num2.intValue();
 		System.out.println("Suma de "+n1+ "+" +n2+ " vale "+(n1+n2));
